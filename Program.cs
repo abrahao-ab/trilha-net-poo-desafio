98de3fbe531b1443c3de8849f167d583b6fb1e81
Emanoel Abrahão Albuquerque Barros <abrahaoab.ti@gmail.com>
2025-01-16 13:46:46 -0300

    Desafio Concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,23 @@
-﻿using DesafioPOO.Models;
+﻿// Program.cs
+using System;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Teste com a classe Nokia
+        Smartphone nokia = new Nokia("123456", "ModeloNokia", "1111111111", 64);
+        Console.WriteLine("--- Teste com Nokia ---");
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        // Teste com a classe iPhone
+        Smartphone iphone = new Iphone("654321", "ModeloIphone", "2222222222", 128);
+        Console.WriteLine("--- Teste com iPhone ---");
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
